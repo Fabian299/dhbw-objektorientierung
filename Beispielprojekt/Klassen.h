d17fc688653bbdd165ab8446acb5d10667231b59
Fabian Schwandner <fabian.schwandner@outlook.de>
2020-10-22 10:10:12 +0200

    Klassen.h eingebunden enum charcater erstellt

--- Beispielprojekt/Klassen.h
+++ Beispielprojekt/Klassen.h
@@ -4,6 +4,8 @@
 #endif // !string
 
 
+typedef enum { Mario, Luigi } character;
+
 class position {
 public:
 	double PosX;
@@ -64,34 +66,19 @@ public:
 
 class Spieler : protected Objekte {
 protected:
-	std::string name;
+	character name;
 public:
-	void set_name(string name) {
-		this->name = name;
+	void set_name(character name) {
+		this->name = name
 	};
-	std::string get_name() {
+	character get_name() {
 		return this->name;
 	};
-	void draw() {
-		int Arr_rot[12][16] = {
-			{0,	0,	0,	255,255,255,255,255,0,	0,	0,	0	},
-			{0, 0,	255,255,255,255,255,255,255,255,255,0	},
-			{0,	0,	130,130,130,239,239,0,	239,0,	0,	0	},
-			{0,	130,239,130,239,239,239,0,	239,239,239,0	},
-			{0,	130,239,130,130,239,239,239,0,	239,239,239	},
-			{0,	130,130,239,239,239,239,0,	0,	0,	0,	0	},
-			{0,	0,	0,	239,239,239,239,239,239,239,0,	0	},
-			{0,	0,	255,255,077,255,255,255,0,	0,	0,	0	},
-			{0,	255,255,255,077,255,255,070,255,255,255,0	},
-			{255,255,255,255,070,070,070,070,255,255,255,255},
-			{239,239,255,070,253,070,070,253,070,255,239,239},
-			{}
-		};
-	}
+	
 
 //Konstruktoren
-	Spieler(std::string name, double Position_X, double Position_Y, double Radius = 0)
+	Spieler(character name = Mario, double Position_X = 0, double Position_Y = 0, double Radius = 0)
 		: Objekte(Position_X, Position_Y, Radius) {
-		this->name = name;
+		set_name(name);
 	};
 };
