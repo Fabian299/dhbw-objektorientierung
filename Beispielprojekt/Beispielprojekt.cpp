319d88064c29a58e90e7691167a6cb81c7da3d11
Fabian Schwandner <fabian.schwandner@outlook.de>
2020-10-27 16:07:24 +0100

    Bewegung hinzugefügt, GameWindow überarbeitet

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -13,12 +13,17 @@
 // Simulationsgeschwindigkeit
 const double DT = 100.0;
 
+character Character = Mario;
+player Figur(Mario, 100, 400, 32);
+game_mode mode = menu;
+
 class GameWindow : public Gosu::Window
 {
 public:
 	Gosu::Image bild;
 	GameWindow()
-		: Window(800, 600)
+		: Window(800, 600),
+		bild(Figur.get_picpath())
 	{
 		set_caption("Gosu Tutorial mit Git");
 	}
@@ -28,22 +33,54 @@ public:
 	// dann werden `draw` Aufrufe ausgelassen und die Framerate sinkt
 	void draw() override
 	{
-		graphics().draw_line(
-			10, 20, Gosu::Color::RED,
-			200, 100, Gosu::Color::GREEN,
-			0.0
-		);
+		switch (mode) {
+		case menu:
+			//Einstellungen, Score, Spielstart
+			break;
+		case start:
+			//Startvorgang
+			break;
+		case running:
+			Gosu::Graphics::draw_rect(
+				0, 400, 800, 200, Gosu::Color::GREEN, 0
+			);
+			bild.draw_rot(
+				Figur.get_PosX(), (Figur.get_PosY() - Figur.get_Radius()), 1, 0, //Position
+				0.5, 0.5,
+				0.1, 0.1 //Skalierung
+			);
+			break;
+		case end:
+			break;
+		default:
+			std::cout << "ERROR: ungueltiger Zustand 'Game_mode'" << std::endl;
+			break;
+		}
 	}
 
 	// Wird 60x pro Sekunde aufgerufen
 	void update() override
 	{
+		if (Gosu::Input::down(Gosu::ButtonName::KB_RIGHT)) {
+			//std::cout << "->" << std::endl;
+			Figur.set_PosX(Figur.get_PosX() + Figur.get_speed() / 60);
+		}
+		if (Gosu::Input::down(Gosu::ButtonName::KB_LEFT)) {
+			//std::cout << "<-" << std::endl;
+			Figur.set_PosX(Figur.get_PosX() - Figur.get_speed() / 60);
+		}
+		if (Gosu::Input::down(Gosu::ButtonName::KB_UP)) {
+			//std::cout << "^" << std::endl;
+			Figur.do_jump();
+		}
+		Figur.flight(400);
 	}
 };
 
 // C++ Hauptprogramm
 int main()
 {
+	mode = running;
 	GameWindow window;
 	window.show();
 }
--- Beispielprojekt/Klassen.h
+++ Beispielprojekt/Klassen.h
@@ -3,6 +3,7 @@
 #include <string>
 #endif // !string
 
+typedef enum {menu, start, running, end} game_mode;
 
 typedef enum { Mario, Luigi } character;
 
@@ -12,14 +13,13 @@ public:
 	double PosY;
 
 //Konstruktoren:
-	position(double Position_X, double Position_Y) {
+	position(double Position_X = 0, double Position_Y = 0) {
 		this->PosX = Position_X;
 		this->PosY = Position_Y;
 	};
-	position() {};
 };
 
-class Objekte {
+class objects {
 protected:
 	double	Radius;
 	position Position;
@@ -31,6 +31,12 @@ public:
 	double get_Radius() {
 		return this->Radius;
 	};
+	double get_PosX() {
+		return this->Position.PosX;
+	};
+	double get_PosY() {
+		return this->Position.PosY;
+	}
 	void set_color(int rot, int gruen, int blau) {
 		this->Farbe.set_red(rot);
 		this->Farbe.set_green(gruen);
@@ -45,18 +51,18 @@ public:
 
 
 //Konstruktoren:
-	Objekte(double Position_X, double Position_Y, double Radius = 0) {
+	objects(double Position_X, double Position_Y, double Radius = 0) {
 		this->Radius = Radius;
 		this->Position.PosX = Position_X;
 		this->Position.PosY = Position_Y;
 	}
-	Objekte(double Position_X, double Position_Y, double Radius, Gosu::Color Farbe) {
+	objects(double Position_X, double Position_Y, double Radius, Gosu::Color Farbe) {
 		this->Radius = Radius;
 		this->Position.PosX = Position_X;
 		this->Position.PosY = Position_Y;
 		this->Farbe = Farbe;
 	}
-	Objekte(double Position_X, double Position_Y, double Radius, int Farbe_rot, int Farbe_gruen, int Farbe_blau) {
+	objects(double Position_X, double Position_Y, double Radius, int Farbe_rot, int Farbe_gruen, int Farbe_blau) {
 		this->Radius = Radius;
 		this->Position.PosX = Position_X;
 		this->Position.PosY = Position_Y;
@@ -64,21 +70,94 @@ public:
 	}
 };
 
-class Spieler : protected Objekte {
+class player : public objects {
 protected:
 	character name;
+	const double j_high = 1.0/9;
+	double j_start = 0;
+	bool jump = FALSE;
+	bool fall = FALSE;
+	std::string picpath;
+	double speed;
+	double j_counter = 0; //Sprung-Zeit-Zähler
 public:
 	void set_name(character name) {
-		this->name = name
+		this->name = name;
+	};
+	void do_jump() {
+		this->jump = TRUE;
+	};
+	void do_fall() {
+		this->fall = TRUE;
+	};
+	void flight(double h_underground = 400){
+		if (this->jump && this->j_counter == 0) {
+			this->j_counter++;
+			this->j_start = this->Position.PosY;
+			this->Position.PosY = this->j_start - (-this->j_high * ((this->j_counter - 30) * (this->j_counter - 30)) + 100);
+		}
+		else if (this->jump && this->j_counter < 30) {
+			this->j_counter++;
+			this->Position.PosY = this->j_start - (-this->j_high * ((this->j_counter - 30) * (this->j_counter - 30))+100);
+		}
+		else if (this->jump && this->j_counter == 30) {
+			this->jump = FALSE;
+			this->fall = TRUE;
+			this->j_counter = 0;
+		}
+
+		if (fall && j_counter == 0 && h_underground > this->Position.PosY) {
+			this->j_counter++;
+			this->j_start = this->Position.PosY;
+			this->Position.PosY = this->j_start + (this->j_high * (this->j_counter * this->j_counter));
+		}
+		else if (fall && h_underground > this->Position.PosY) {
+			this->j_counter++;
+			this->Position.PosY = this->j_start + (this->j_high * (this->j_counter * this->j_counter));
+		}
+		else if (fall) {
+			this->fall = FALSE;
+			this->j_counter = 0;
+		}
+		//debug
+		if (fall | jump) {
+			std::cout << this->j_counter << " | " << this->Position.PosY << std::endl;
+		}
+	}
+	void set_PosX(double PosX) {
+		this->Position.PosX = PosX;
+	}
+	void set_PosY(double PosY) {
+		this->Position.PosY = PosY;
+	}
+	void set_speed(double speed) {
+		this->speed = speed;
+	}
+	double get_speed() {
+		return speed;
 	};
 	character get_name() {
 		return this->name;
 	};
-	
+	std::string get_picpath() {
+		switch (name) {
+		case Mario:
+			return "Mario.png";
+			break;
 
+		case Luigi:
+			return "Luigi.png";
+			break;
+
+		default:
+			printf("\nERROR: ungültiger Zustand\n");
+		};
+	}
+	
 //Konstruktoren
-	Spieler(character name = Mario, double Position_X = 0, double Position_Y = 0, double Radius = 0)
-		: Objekte(Position_X, Position_Y, Radius) {
+	player(character name = Mario, double Position_X = 0, double Position_Y = 0, double Radius = 0, double speed = 80)
+		: objects(Position_X, Position_Y, Radius) {
 		set_name(name);
+		set_speed(speed);
 	};
 };
