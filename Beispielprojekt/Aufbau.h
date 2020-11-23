c1ebcff4abd269c83b081df6e30b7279190206f8
Silas Eisenkolb <s-eisenkolb@web.de>
2020-11-23 22:32:48 +0100

    Late Night Überarbeitung

--- Beispielprojekt/Aufbau.h
+++ Beispielprojekt/Aufbau.h
@@ -8,94 +8,114 @@
 #include <Windows.h>
 #include <string>
 
-class User {
-	// Silas
-	int score;
-	std::string player_name;
-public:
-	void set_score(const int& s) {
-		this->score = s;
-	}
-
-	void set_name(const std::string& n) {
-		this->player_name = n;
-	}
-
-	std::string get_playername() {
-		return this->player_name;
-	}
-
-	int get_score() {
-		return this->score;
-	}
-};
-
-class Settings : public User {
-	int gamespeed;
-	int victim_color;
-public:
-	void set_gamespeed(const int& gs) {
-		this->gamespeed = gs;
-	}
-	void set_victim_color(const int& vc) {
-		this->victim_color = vc;
-	}
-
-	int get_gamespeed() {
-		return this->gamespeed;
-	}
-	int get_victim_color() {
-		return this->victim_color;
-	}
-	// aus Elternklasse
-	void set_changedname(const std::string &changedname) {
-		this->set_name(changedname);	// Hallo Fabian
-	}
-
-	std::string get_changendname() {
-		return this->get_playername();
-	}
-};
-
-
-
-
-
-
-
-/*
-
 using namespace std;
 
-// Globale Variablen
-//int color_victims;
-//int gamespeed;
+class User {
+	private:
+		// Silas
+		int score;
+		string player_name;
+	public:
+		User() {};
+		~User() {};
 
-struct playerscore {
-	int score;
-	string name;
+		void set_score(const int s);
+		int get_score();
+		void set_name(const std::string n);
+		string get_name();
 };
 
-// Überladen des Streamingoperators
-// istream& operator>>(istream& stream, playerscore& rl);
 
-// Funktionsdeklaration
-string ablauf_start();
-void ablauf_mainmenue();
-void ablauf_game();
-string ablauf_changename();
-void ablauf_ranking(vector<playerscore> ranking_list);
-void ablauf_setting();
-void ablauf_exit();
+class Settings  {
+	private:
+		int gamespeed;
+		int victim_color;
+		//int TopTenRanking [10][2];
+		int TopTenRanking[10];
+		string TopTenPlayer[10];
+	public:
+		Settings() {	// Konstruktor
+			gamespeed = 10;
+			victim_color = 2;
+			TopTenRanking= { 0,0,0,0,0,0,0,0,0,0 };
+			TopTenPlayer={"Silas", "Sarah", "Silas", "Sarah", "Silas", "Sarah","Silas", "Sarah", "Silas", "Sarah"};
+		};
+		~Settings(){};
 
-void fkt_navigation(int ort);
-// bool fkt_check_name(string new_name);
-void fkt_reset_rankinglist();
-void fkt_after_the_game(int value, string name, vector<playerscore>& a);
-void fkt_print_ranking(vector<playerscore>& a);
-void fkt_rankingsort(vector<playerscore>& a);
-// void fkt_init_from_file(vector<playerscore>& a);
-// void fkt_update_the_file(vector<playerscore>& a);
-void fkt_change_character();
-void fkt_get_playername();
-*/
+		int get_gamespeed();
+		void set_gamespeed(const int gs);
+		int get_victim_color();
+		void set_victim_color(const int vc);
+		void set_rankinglist(const int Userscore, const char Username);
+		int get_rankinglist();
+		void print_rankinglist();
+		
+}
+
+// 		void set_gamespeed(const int gs) {
+// 		this->gamespeed = gs;
+// 	}
+// 		void set_victim_color(const int vc) {
+// 		this->victim_color = vc;
+// 	}
+
+// 		int get_gamespeed() {
+// 		return this->gamespeed;
+// 	}
+// 		int get_victim_color() {
+// 		return this->victim_color;
+// 	}
+
+// };
+
+// void set_score(const int& s) {
+// 		this->score = s;
+// 	}
+
+// void set_name(const std::string& n) {
+// 		this->player_name = n;
+// 	}
+// std::string get_playername() {
+// 		return this->player_name;
+// 	}
+
+// 	int get_score() {
+// 		return this->score;
+// 	}
+
+// /*
+
+// using namespace std;
+
+// // Globale Variablen
+// //int color_victims;
+// //int gamespeed;
+
+// struct playerscore {
+// 	int score;
+// 	string name;
+// };
+
+// // Überladen des Streamingoperators
+// // istream& operator>>(istream& stream, playerscore& rl);
+
+// // Funktionsdeklaration
+// string ablauf_start();
+// void ablauf_mainmenue();
+// void ablauf_game();
+// void ablauf_changename();
+// void ablauf_ranking(vector<playerscore> ranking_list);
+// void ablauf_setting();
+// void ablauf_exit();
+
+// void fkt_navigation(int ort);
+// // bool fkt_check_name(string new_name);
+// void fkt_reset_rankinglist();
+// void fkt_after_the_game(int value, string name, vector<playerscore>& a);
+// void fkt_print_ranking(vector<playerscore>& a);
+// void fkt_rankingsort(vector<playerscore>& a);
+// // void fkt_init_from_file(vector<playerscore>& a);
+// // void fkt_update_the_file(vector<playerscore>& a);
+// void fkt_change_character();
+// void fkt_get_playername();
+// */
--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -3,78 +3,108 @@
 #include "Vektor2d.h"
 #include "objects.h"
 #include "Aufbau.h"
-
+#include <iostream>
+using namespace std;
 // Simulationsgeschwindigkeit
 const double DT = 100.0;
 
 // vector<playerscore> ranking_list = { };
 // string player_name;
 
-// Silas
-std::string a1 = "With the '1' you can start the game. With the '2' you go to the settings. With the '0' you exit the programm. ";
+/* Sarah Testet Sachen */
 
+int main(){
+	Settings mySetting();
+	mySetting.set_victim_color(3)
+	cout << mySetting.get_victim_color()<<endl;
+	mySetting.set_gamespeed(10000);
+	cout << mySetting.get_gamespeed()<<endl;
 
-
-character Character = Mario;
-player Figur(Mario, 100, 400, 32, 130);
-uint16_t score = 0;
-
-class GameWindow : public Gosu::Window
-{
-public:
-	Gosu::Image bild;
-	GameWindow()
-		: Window(800, 600)
-	{
-		set_caption("Fabian and Silas wonderfull Game");
-	}
-
-	// wird bis zu 60x pro Sekunde aufgerufen.
-	// Wenn die Grafikkarte oder der Prozessor nicht mehr hinterherkommen,
-	// dann werden `draw` Aufrufe ausgelassen und die Framerate sinkt
-	void draw() override
-	{
-		Gosu::Graphics::draw_rect(0, 400, 800, 200, Gosu:: Color::Color(20, 200, 20), 0);
-		Figur.draw();
-		//Gosu::Font(string_menü);
-		/*switch (mode) {
-
-			case
-			default
-		}*/
-	}
-
-	// Wird 60x pro Sekunde aufgerufen
-	void update() override
-	{
-		if (Gosu::Input::down(Gosu::ButtonName::KB_LEFT_CONTROL)) {
-			Figur.set_boost();
-		}
-		else {
-			Figur.reset_boost();
-		}
-		if (Gosu::Input::down(Gosu::ButtonName::KB_RIGHT)) {
-			//std::cout << "->" << std::endl;
-			Figur.set_PosX(Figur.get_PosX() + Figur.get_speed() / 60);
-			score++;
-		}
-		if (Gosu::Input::down(Gosu::ButtonName::KB_LEFT)) {
-			//std::cout << "<-" << std::endl;
-			Figur.set_PosX(Figur.get_PosX() - Figur.get_speed() / 60);
-		}
-		if (Gosu::Input::down(Gosu::ButtonName::KB_UP)) {
-			//std::cout << "^" << std::endl;
-			Figur.do_jump();
-		}
-		Figur.flight(400);
-	}
-};
-
-// C++ Hauptprogramm
-int main()
-{
-//	ablauf_start();
-//	mode = running;
-//	GameWindow window;
-//	window.show();
 }
+
+// void get_gamespeed();
+		
+// void set_gamespeed(const int gs);
+// void get_victim_color();
+		
+// void set_victim_color(const int vc);
+// void set_rankinglist(const int Userscore, const char Username);
+// int get_rankinglist();
+// void print_rankinglist();
+
+// void set_score(const int s);
+// void get_score();
+// void set_name(const std::string n);
+// void get_name();
+
+
+
+
+// // Silas
+// std::string a1 = "With the '1' you can start the game. With the '2' you go to the settings. With the '0' you exit the programm. ";
+
+
+
+// character Character = Mario;
+// player Figur(Mario, 100, 400, 32, 130);
+// uint16_t score = 0;
+
+// class GameWindow : public Gosu::Window
+// {
+// public:
+// 	Gosu::Image bild;
+// 	GameWindow()
+// 		: Window(800, 600)
+// 	{
+// 		set_caption("Fabian and Silas wonderfull Game");
+// 	}
+
+// 	// wird bis zu 60x pro Sekunde aufgerufen.
+// 	// Wenn die Grafikkarte oder der Prozessor nicht mehr hinterherkommen,
+// 	// dann werden `draw` Aufrufe ausgelassen und die Framerate sinkt
+// 	void draw() override
+// 	{
+// 		Gosu::Graphics::draw_rect(0, 400, 800, 200, Gosu:: Color::Color(20, 200, 20), 0);
+// 		Figur.draw();
+// 		//Gosu::Font(string_menü);
+// 		/*switch (mode) {
+
+// 			case
+// 			default
+// 		}*/
+// 	}
+
+// 	// Wird 60x pro Sekunde aufgerufen
+// 	void update() override
+// 	{
+// 		if (Gosu::Input::down(Gosu::ButtonName::KB_LEFT_CONTROL)) {
+// 			Figur.set_boost();
+// 		}
+// 		else {
+// 			Figur.reset_boost();
+// 		}
+// 		if (Gosu::Input::down(Gosu::ButtonName::KB_RIGHT)) {
+// 			//std::cout << "->" << std::endl;
+// 			Figur.set_PosX(Figur.get_PosX() + Figur.get_speed() / 60);
+// 			score++;
+// 		}
+// 		if (Gosu::Input::down(Gosu::ButtonName::KB_LEFT)) {
+// 			//std::cout << "<-" << std::endl;
+// 			Figur.set_PosX(Figur.get_PosX() - Figur.get_speed() / 60);
+// 		}
+// 		if (Gosu::Input::down(Gosu::ButtonName::KB_UP)) {
+// 			//std::cout << "^" << std::endl;
+// 			Figur.do_jump();
+// 		}
+// 		Figur.flight(400);
+// 	}
+// };
+
+// // C++ Hauptprogramm
+// int main()
+// {
+// //	ablauf_start();
+// //	mode = running;
+// //	GameWindow window;
+// //	window.show();
+// }
