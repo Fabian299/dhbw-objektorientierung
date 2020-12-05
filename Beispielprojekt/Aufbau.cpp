98da33e75d376a7549570758c941dc50e16c75cb
Silas Eisenkolb <s-eisenkolb@web.de>
2020-12-05 13:28:51 +0100

    Debuggen

--- Beispielprojekt/Aufbau.cpp
+++ Beispielprojekt/Aufbau.cpp
@@ -1,6 +1,7 @@
+
 #include "stdafx.h"
 #include "Aufbau.h"
-#include "objects.H"
+
 
 
 using namespace std;
@@ -9,317 +10,75 @@ using namespace std;
 typedef enum { mainmenue, game, changeplayer, ranking, setting } enum_ort;
 
 // Definitionen der Klassenmethode
-void User::set_score(const int s){
-	this->score = s;
+void User::set_score(const int s) {
+    this->score = s;
 };
-int User::get_score(){
-	return this->score;
+int User::get_score() {
+    return this->score;
 };
-void User::set_name(const std::string n){
-	this->player_name = n;
+void User::set_name(const std::string n) {
+    this->player_name = n;
 };
-string User::get_name(){
-	return this->player_name;
+string User::get_name() {
+    return this->player_name;
 };
 
 
 // ---------------------Settings ---------------------------------
 
 
-int Settings::get_gamespeed(){
-	return this->gamespeed;
+int Settings::get_gamespeed() {
+    return this->gamespeed;
 }
-void Settings::set_gamespeed(const int gs){
-	this->gamespeed = gs;
+void Settings::set_gamespeed(const int gs) {
+    this->gamespeed = gs;
 }
-int Settings::get_victim_color(){
-	return this->victim_color;
+int Settings::get_victim_color() {
+    return this->victim_color;
 }
-void Settings::set_victim_color(const int vc){
-	this->victim_color = vc;
+void Settings::set_victim_color(const int vc) {
+    this->victim_color = vc;
 }
 
+bool Settings::get_charakter() {
+    if (this->character)
+        return true;
+    else
+        return false;
+}
+
+void Settings::set_charakter(bool charakter) {
+    this->character = charakter;
+}
 /*
-void Settings::set_rankinglist(const int Userscore, char Username) {
-	//int TopTen[] = this->TopTenRanking; 
-	if (Userscore > this->TopTenRanking[9][1]){	// Name , Score
-		// einfügen
-		for(int i = 8; i>=0; i--){
-			if(Userscore<this->TopTenRanking[i]) {     
-				this->TopTen[i+1]=Userscore;
-				this->TopTenPlayer[i+1]=Username;  //geht das?
-				i=0;
-				break;  // reicht 1 break um aus der Vorschleife zu kommen?
-			}
-		}
-		// break;
-	}
-	else
-	{
-		cout <<"You are stupid as fuck" << endl;	// Ausgabe noch freundlich machen
-	}
-	//this->TopTenRanking = TopTen,
+void Settings::set_rankinglist(const int Userscore) {
+    int TopTen[] = this->TopTenRanking;
+    if (Userscore > this->TopTenRanking[9][1]){	// Name , Score
+        // einfügen
+        for(int i = 8; i>=0; i--){
+            if(Userscore<this->TopTenRanking[i]) {
+                this->TopTen[i+1]=Userscore;
+                i=0;
+                break;  // reicht 1 break um aus der Vorschleife zu kommen?
+            }
+        }
+        // break;
+    }
+    else
+    {
+        cout <<"You are stupid as fuck" << endl;	// Ausgabe noch freundlich machen
+    }
+    //this->TopTenRanking = TopTen,
 };
 
-int Settings::get_rankinglist(){ 
-	return this->TopTenPlayer, this->TopTenRanking;
-} 
+int[10] Settings::get_rankinglist(){
+    return this->TopTenRanking;
+}
 
 int Settings::print_rankinglist(){
-	int TopTenNames[10];
-	int TopTenScores[10];
-	TopTenNames, TopTenScores = get_rankinglist();
-   	for (int i = 0; i < 10; i++) {
-	   cout << "Name: " << TopTenNames[i] << "\tScore: " << TopTenScores[i] << endl;
-	}	
-}
-*/
-// /*
-// int main() {
-// 	ablauf_start();
-
-// 	// After game 
-// 	// /*
-// 	int value = 3;
-// 	fkt_after_the_game(value, player_name, ranking_list);
-// 	// 
-
-// }	*/
-
-
-// // Globale Variablen
-
-// // Überladen des Streamingoperators 
-// /*
-// istream& operator>>(istream& stream, playerscore& rl) {
-// 	stream >> rl.score;
-// 	char op;
-// 	stream >> op;
-// 	assert(op == '\t');
-// 	stream >> rl.name;
-// 	// char op2;				// Evtl wenn man in txt auch Iterator haben möchte, als "Ranking" in der txt-Datei
-// 	// assert(op2 == "\t");
-// 	return stream;
-// } */
-
-
-
-// // Funktionsdefinition
-// void ablauf_start() {
-// 	cout << "Das Spiel wird geöffnet." << endl;
-// 	//	ifstream f("Rankinglist.txt");
-// 	//	if (f) fkt_init_from_file(ranking_list);
-// 	cout << "Bitte warten" << endl;
-// 	// _sleep(2);
-// 	cout << "..." << endl;
-// 	// _sleep(2);
-// 	cout << "..." << endl;
-// 	// _sleep(2);
-// 	cout << "Bitte geben Sie ihren Namen ein" << endl;
-// 	string player_name; 
-// 	cin >> player_name;
-// 	User::set_name(player_name);
-// 	// ---------------------
-// 	ablauf_mainmenue();
-// }
-
-// void ablauf_mainmenue() {
-// 	enum_ort Ort = mainmenue; // 1
-// 	fkt_navigation(Ort);
-// 	cout << "With the '1' you can start the game.\nWith the '2' you can change your name." << endl;
-// 	cout << "With the '3' you can see the best player.\n With the '4' you can change the settings" << endl;
-// 	cout << "With the '0' you exit the programm. \n\n" << endl;
-// 	// _sleep(1);
-// 	cout << "\n What do you want?" << endl;
-// 	char eingabe = cin.get();
-// 	if (eingabe == '1') ablauf_game();
-// 	if (eingabe == '2') ablauf_changename();
-// 	if (eingabe == '3') ablauf_ranking();
-// 	if (eingabe == '4') ablauf_setting();
-// 	if (eingabe == '0') ablauf_exit();
-// 	else ablauf_mainmenue();
-// }
-
-// void ablauf_game() {
-// 	enum_ort Ort = game; // 2
-// 	fkt_navigation(Ort);
-// 	//	mode = running
-// 	//	GameWindow window;
-// 	//	window.show();
-// }
-
-// void ablauf_changename() {
-// 	enum_ort Ort = changeplayer; // 3
-// 	fkt_navigation(Ort);
-
-// 	string changed_name;
-// 	cin >> changed_name;
-// 	//	if (fkt_check_name(changed_name)) ablauf_changename();		// Fehler bei 1 --> Neustart der Seite
-
-// 	cout << "Change successfull" << endl;
-// 	//		_sleep(1);
-// 	User::set_name(changed_name);
-// 	ablauf_mainmenue();	//		-------------??????---------------
-// }
-
-// void ablauf_ranking(vector<playerscore> ranking_list) {
-// 	enum_ort Ort = ranking; // 4
-// 	fkt_navigation(Ort);
-
-// 	fkt_print_ranking(ranking_list);
-// 	system("pause");
-// 	ablauf_mainmenue();
-// }
-
-// void ablauf_setting() {
-// 	enum_ort Ort = setting; // 5
-// 	fkt_navigation(Ort);
-// 	cout << "Press '1' to reset the rankinglist. Press '2' to change the character.\nPress '0' for exit the settings" << endl;
-// 	cout << "Press 'L' to use Luigi or 'M' to use Mario" << endl;
-
-// 	char eingabe = cin.get();
-// 	if (eingabe == '1') fkt_reset_rankinglist();
-// 	if (eingabe == '2') fkt_change_character();
-// 	if (eingabe == '3') ablauf_mainmenue();
-// 	if (eingabe == 'l' || eingabe == 'L') character = Luigi; // Luigi
-// 	if (eingabe == 'm' || eingabe == 'M') character = Mario; // Mario
-// 	else ablauf_setting();
-
-// 	cout << "\n\n\n!!HIER GEHT NOCH NICHTS" << endl;
-// }
-
-// void ablauf_exit() {
-// 	cout << "You exit the game. Thank you for using it. We hope we will see you another time.\n" << endl;
-// 	cout << "Best regard from Fabian and Silas!" << endl;
-// 	// _sleep(1);
-// //	void exit(					// Programm schließen
-// //		int const status
-// //	);
-// }
-
-// void fkt_navigation(int ort) {
-// 	system("cls");
-// 	switch (ort)
-// 	{
-// 	case 1:	cout << "Mainmenue" << endl;
-// 		cout << "----------\n\n" << endl;
-
-// 	case 2: cout << "Game - please change the window" << endl;
-// 		cout << "----------\n\n" << endl;
-// 	case 3: cout << "You are changing your name. What's your name?" << endl;
-// 		cout << "----------\n\n" << endl;
-// 	case 4: cout << "Ranking - Who's the best player?" << endl;
-// 		cout << "----------\n\n" << endl;
-// 	case 5: cout << "Settings" << endl;
-// 		cout << "----------\n\n" << endl;
-// 	default: cout << endl;
-// 	}
-// }
-
-// /* bool fkt_check_name(string new_name) {
-// 	for (int i = 0; i < strlen(new_name); i++)
-// 	{
-// 		if (new_name[i] == '\t') return true;
-// 		else ++i;
-// 	}
-// 	return false;
-// } */
-
-// void fkt_reset_rankinglist() {
-// 	//	ofstream f("Rankinglist.txt", ios::trunc);		//ios::trunc leert die Datei
-// 	cout << "Rankinglist is reseted" << endl;
-// }
-
-// void fkt_after_the_game(int value, string name, vector<playerscore>& a) {
-// 	if (value > a.at(9).score) cout << "You are not in the Top 10" << endl;
-// 	else {
-// 		fkt_rankingsort(a);
-// 		if (value == a.at(0).score) cout << "Congratulation. You are the HIGHSCORE !!" << endl;
-// 		else cout << "Congratulation! You are in the Top 10" << endl;
-// 		playerscore new_entry = { value, name };				// des evtl schon außerhalb, anderer Name der Fkt..
-// 		a.push_back(new_entry);
-// 		fkt_rankingsort(a);
-// 		// fkt_update_the_file();
-// 	}
-
-// 	while (a.size() >= 100) a.pop_back();	// Nur besten 100, schlechter wird gelöscht
-// }
-
-// void fkt_print_ranking(const vector <playerscore>& a)
-// {
-// 	for (int elem = 1; elem < 11; elem++)
-// 	{
-// 		if (elem < a.size()) cout << elem << ". Platz\t" << a.at(elem).name << "\t Score: " << a.at(elem).score << endl;
-// 	}
-// }
-
-// void fkt_rankingsort(vector <playerscore>& a)
-// {
-// 	bool sorted = false;
-// 	//n-1 Sortierdurchläufe von n-1 bis 1
-// 	size_t j = a.size() - 1;
-// 	while ((j > 0) && (sorted == false))
-// 	{
-// 		sorted = true; //Annahme, a sei schon sortiert
-// 		//Ein Sortierdurchlauf von 0 bis j-1
-// 		for (size_t i = 0; i < j; i++)
-// 		{
-// 			if (a.at(i + 1).score < a.at(i).score)
-// 			{
-// 				//Elemente i und i+1 vertauschen
-// 				int temp = a.at(i).score;
-// 				a.at(i).score = a.at(i + 1).score;
-// 				a.at(i + 1).score = temp;
-// 				//da Vertauschung notwendig, ist a evtl. noch nicht sortiert
-// 				sorted = false;
-// 			}
-// 		}
-// 		j = j - 1;
-// 	}
-// }
-
-// // Ranglistenvector aus Textdatei initialisieren --> Am Anfang des Programms (-start)
-// /*
-// fkt_init_from_file(vector<playerscore>& a) {
-// 	ifstream f("Rankinglist.txt");
-// 	if (!f) cout << "Error. Can't open the file." << endl;
-// 	else {   // Lesen ...
-// 		a.clear();
-// 		string line;
-// 		while (getline(f, line))
-// 		{
-// 			playerscore tmp;
-// 			line >> tmp;
-// 			a.push_back(tmp);
-// 		}
-// 	}
-// }	*/
-
-// // Vector ergänzen bzw kompletter (geordneter) Vector überschreiben.
-
-// /*
-// void fkt_update_the_file(vector<playerscore>& a) {
-// //	ofstream f("Rankinglist.txt", ios::trunc);		// Datei leermachen											// Vector schreiben
-// 		for (vector<playerscore> vec : a)
-// 		{
-// //			f << vec.score << "\t" << vec.name << endl;
-// 		}
-
-// } 
-
-// void fkt_change_character() {
-// 	system("cls");
-// 	cout << "You want change your character?" << endl;
-// 	cout << "Press '1' for Mario, Press '2' for Luigi.\nPress '0' to return to the settings." << endl;
-// 	char eingabe = cin.get();
-// 	if (eingabe == '1') // character = Mario;
-// 		if (eingabe == '2') // character = Luigi;
-// 			if (eingabe == '0') ablauf_setting();
-// 			else fkt_change_character();
-// }
-
-// void fkt_get_playername() {
-	
-// }
-
-
+    int TopTenScores[10];
+    TopTenScores = get_rankinglist();
+    for (int i = 0; i < 10; i++) {
+        cout << "\tScore: " << TopTenScores[i] << endl;
+    }
+}*/
--- Beispielprojekt/Aufbau.h
+++ Beispielprojekt/Aufbau.h
@@ -1,5 +1,5 @@
-// #include "Aufbau.h"
-#include "stdafx.h"
+
+//#include "stdafx.h"
 #include <iostream>
 #include <thread>
 #include <string>
@@ -11,111 +11,52 @@
 using namespace std;
 
 class User {
-	private:
-		// Silas
-		int score;
-		string player_name;
-	public:
-		User() {};
-		~User() {};
+private:
+    // Silas
+    int score;
+    string player_name;
+public:
+    User() {
+        score = 0;
+        player_name = "Hansi";
+    };
+    ~User() {};
 
-		void set_score(const int s);
-		int get_score();
-		void set_name(const std::string n);
-		string get_name();
+    void set_score(const int s);
+    int get_score();
+    void set_name(const std::string n);
+    string get_name();
 };
 
 
-class Settings  {
-	private:
-		int gamespeed;
-		int victim_color;
-		//int TopTenRanking [10][2];
-		int TopTenRanking[10];
-		string TopTenPlayer[10];
-	public:
-		Settings() {	// Konstruktor
-			gamespeed = 10;
-			victim_color = 2;
-			TopTenRanking= { 0,0,0,0,0,0,0,0,0,0 };
-			TopTenPlayer={"Silas", "Sarah", "Silas", "Sarah", "Silas", "Sarah","Silas", "Sarah", "Silas", "Sarah"};
-		};
-		~Settings(){};
+class Settings {
+private:
+    int gamespeed;
+    int victim_color;
+    bool character; // 0 - Mario, 1- Luigi
+    //int TopTenRanking[10];
+    //string TopTenPlayer[10];
+public:
+    Settings() {	// Konstruktor
+        gamespeed = 10;
+        victim_color = 2;
+        character = 0;
+        //TopTenRanking= { 0,0,0,0,0,0,0,0,0,0 };
+        //TopTenPlayer={"Silas", "Fabse", "Silas", "Fabse", "Silas", "Fabse","Silas", "Fabse", "Silas", "Fabse"};
+    };
+    ~Settings() {};
 
-		int get_gamespeed();
-		void set_gamespeed(const int gs);
-		int get_victim_color();
-		void set_victim_color(const int vc);
-		void set_rankinglist(const int Userscore, const char Username);
-		int get_rankinglist();
-		void print_rankinglist();
-		
-}
+    int get_gamespeed();
+    void set_gamespeed(const int gs);
+    int get_victim_color();
+    void set_victim_color(const int vc);
+    void set_charakter(bool charakter);
+    bool get_charakter();
 
-// 		void set_gamespeed(const int gs) {
-// 		this->gamespeed = gs;
-// 	}
-// 		void set_victim_color(const int vc) {
-// 		this->victim_color = vc;
-// 	}
+    //void set_rankinglist(const int Userscore, const char Username);
+    //int get_rankinglist();
+    //void print_rankinglist();
 
-// 		int get_gamespeed() {
-// 		return this->gamespeed;
-// 	}
-// 		int get_victim_color() {
-// 		return this->victim_color;
-// 	}
 
-// };
+};
 
-// void set_score(const int& s) {
-// 		this->score = s;
-// 	}
-
-// void set_name(const std::string& n) {
-// 		this->player_name = n;
-// 	}
-// std::string get_playername() {
-// 		return this->player_name;
-// 	}
-
-// 	int get_score() {
-// 		return this->score;
-// 	}
-
-// /*
-
-// using namespace std;
-
-// // Globale Variablen
-// //int color_victims;
-// //int gamespeed;
-
-// struct playerscore {
-// 	int score;
-// 	string name;
-// };
-
-// // Überladen des Streamingoperators
-// // istream& operator>>(istream& stream, playerscore& rl);
-
-// // Funktionsdeklaration
-// string ablauf_start();
-// void ablauf_mainmenue();
-// void ablauf_game();
-// void ablauf_changename();
-// void ablauf_ranking(vector<playerscore> ranking_list);
-// void ablauf_setting();
-// void ablauf_exit();
-
-// void fkt_navigation(int ort);
-// // bool fkt_check_name(string new_name);
-// void fkt_reset_rankinglist();
-// void fkt_after_the_game(int value, string name, vector<playerscore>& a);
-// void fkt_print_ranking(vector<playerscore>& a);
-// void fkt_rankingsort(vector<playerscore>& a);
-// // void fkt_init_from_file(vector<playerscore>& a);
-// // void fkt_update_the_file(vector<playerscore>& a);
-// void fkt_change_character();
-// void fkt_get_playername();
-// */
